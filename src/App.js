64ca2665b71ac888888220779b06be4b0cdd0767
dav-cho <chodavid@gmail.com>
2021-04-29 12:29:37 -0400

    remove search context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route } from 'react-router-dom';
 
 import { ThemeContextProvider } from './contexts/theme.context';
-import { SearchContextProvider } from './contexts/search.context';
 import { Header } from './components/header/header.component';
 import Home from './pages/home/home.component';
 import About from './pages/about/about.component';
@@ -12,16 +11,14 @@ import './App.scss';
 function App() {
   return (
     <ThemeContextProvider>
-      <SearchContextProvider>
-        <div className="App">
-          <Header />
-          <div className="content">
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/search/:searchVal" component={Results} />
-          </div>
+      <div className="App">
+        <Header />
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/search/:searchVal" component={Results} />
         </div>
-      </SearchContextProvider>
+      </div>
     </ThemeContextProvider>
   );
 }
